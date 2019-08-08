b8e41f0395e6626ecf00b4d17cfd229f47ddaf78
hushirui <hushirui-hb@bestpay.com.cn>
2019-08-08 23:40:43 +0800

    .

--- spring.starter.dao/src/main/java/com/mrathena/dao/toolkit/DynamicDataSourceHolder.java
+++ spring.starter.dao/src/main/java/com/mrathena/dao/toolkit/DynamicDataSourceHolder.java
@@ -1,6 +1,9 @@
 package com.mrathena.dao.toolkit;
 
 /**
+ * 针对线程有效,而且要放在事务之前(不然事务不生效),放在service或者biz的开头就可以了
+ * DynamicDataSourceHolder.set(DynamicDataSource.Key.SHARDING);
+ *
  * @author mrathena on 2019/8/8 22:17
  */
 public class DynamicDataSourceHolder {
