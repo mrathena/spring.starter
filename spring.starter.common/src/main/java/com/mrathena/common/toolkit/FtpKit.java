bace6e44e7eab421b5f2db6c61eb6d75905b97e6
mrathena <admin@mrathena.com>
2019-09-07 21:32:17 +0800

    .

--- spring.starter.common/src/main/java/com/mrathena/common/toolkit/FtpKit.java
+++ spring.starter.common/src/main/java/com/mrathena/common/toolkit/FtpKit.java
@@ -3,7 +3,7 @@ package com.mrathena.common.toolkit;
 /**
  * @author mrathena on 2019/5/27 11:44
  */
-public class FtpKit {
+public final class FtpKit {
 
 	public static void main(String[] args) {
 		// TODO
