fbf9d010731e62a4c2914a798734a42d5457940c
mrathena <admin@mrathena.com>
2019-09-15 19:05:41 +0800

    .

---
+++ spring.starter.api/src/main/java/com/mrathena/spring/starter/api/toolkit/check/CheckKit.java
@@ -0,0 +1,12 @@
+package com.mrathena.spring.starter.api.toolkit.check;
+
+/**
+ * @author mrathena on 2019-09-15 12:55
+ */
+public final class CheckKit {
+
+	public static <T> void check(T request) {
+
+	}
+
+}
